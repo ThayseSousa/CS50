dfbed7b4821be67f4d4377b1b230066ca49a2087
Thayse Cristina de Sousa <thaayse-cristina@hotmail.com>
2020-02-13 21:47:51 -0300

    Atualização

---
+++ Python/list.py
@@ -0,0 +1,7 @@
+import os
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+
+engine = create_engine(os.getenv("DATABASE_URL"))
+db = scoped_session(sessionmaker(blind=engine))
